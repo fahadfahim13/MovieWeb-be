687454336e1b7634976c5cc1f4fb142e1aeb7ba0
fahadfahim13 <fahadfahim13@gmail.com>
2022-09-22 21:04:28 +0600

    Added jwtguard to all apis

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -7,25 +7,30 @@ import {
   Param,
   Delete,
   Req,
+  UseGuards,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { JwtGuard } from '../auth/guards/jwt.guard';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
+  @UseGuards(JwtGuard)
   @Post()
   create(@Body() createMovieDto: CreateMovieDto) {
     return this.moviesService.create(createMovieDto);
   }
 
+  @UseGuards(JwtGuard)
   @Get()
   findAll() {
     return this.moviesService.findAll();
   }
 
+  @UseGuards(JwtGuard)
   @Post('search')
   async search(
     @Body() body: { value?: string; page?: number; limit?: number },
@@ -46,16 +51,19 @@ export class MoviesController {
     };
   }
 
+  @UseGuards(JwtGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.moviesService.findOne(id);
   }
 
+  @UseGuards(JwtGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateMovieDto: UpdateMovieDto) {
     return this.moviesService.update(id, updateMovieDto);
   }
 
+  @UseGuards(JwtGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.moviesService.remove(id);
